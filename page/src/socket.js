53596ecf07f973643920e84e9344410a4dfeef5f
luka598 <luka598@tuta.io>
2023-10-13 09:21:38 +0000

    Reconnect

--- page/src/socket.js
+++ page/src/socket.js
@@ -3,4 +3,8 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:3000';
 const URL = "http://localhost:3000"
-export const socket = io(URL);
+export const socket = io(URL, {
+    'reconnection': true,
+    'reconnectionDelay': 500,
+    'reconnectionAttempts': 10
+  });
